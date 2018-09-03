a498d532a5e824cc160e67074a79c7eee9c316af
Mohamed Wael Khobalatte <wael@pacelink.de>
2018-09-03 11:36:57 +0200

    Introduce oauth2 middlware for PACE services
    The middleware is responsible to introspecting the token (that is, asking
    the PACE ID service whether this token is active or not), and if so, puts
    various details in the context and passes it down to the caller.

---
+++ oauth2/oauth2.go
@@ -0,0 +1,155 @@
+package oauth2
+
+import (
+  "fmt"
+  "strings"
+  "net/http"
+  "net/url"
+  "context"
+  "io/ioutil"
+  "encoding/json"
+)
+
+type Middleware struct {
+  Host string         
+  ClientID string
+  ClientSecret string
+}
+
+type IntrospectResponse struct {
+    Status bool `json:"active"`
+    Scope *string `json:"scope"` // Could be string or nil, hence the pointer.
+    ClientID string `json:"client_id"` 
+}
+
+// Should take token, introspect it, and put the token and other relevant information back 
+// in the context.
+// TODO
+// 
+// 1. Move error code to well named functions. 
+// 2. Refaactor code that puts things in the context.
+func (m *Middleware) Middleware(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+      qualifiedToken := r.Header.Get("Authorization")
+
+      items := strings.Split(qualifiedToken, "Bearer ")
+      if len(items) < 2 {
+        // TODO
+        // 
+        // Should these responses actually follow JSON spec?
+        http.Error(w, "Unauthorized", http.StatusUnauthorized)
+        return
+      }
+
+      token := items[1]
+
+      resp, err := http.PostForm(m.Host + "/oauth2/introspect",
+        url.Values{"client_id": {m.ClientID}, "client_secret": {m.ClientSecret}, "token": {token}})
+
+      defer resp.Body.Close()
+
+      if err != nil {
+        // Possible upstream error, log and fail. Use log instead of fmt?
+        fmt.Printf("%v\n", err)
+        http.Error(w, "InternalServerError", http.StatusInternalServerError)
+        return
+      }
+
+      body, err := ioutil.ReadAll(resp.Body)
+      if err != nil {
+        fmt.Printf("%v\n", err)
+        http.Error(w, "InternalServerError", http.StatusInternalServerError)
+        return 
+      }
+
+      // If Response is not 200, it means there are problems with setup, such 
+      // as wrong client ID or secret. 
+      if resp.StatusCode != 200 {
+        fmt.Printf("Received %s from server, most likely bad oauth config.\n", resp.StatusCode)
+
+        http.Error(w, "InternalServerError", http.StatusInternalServerError)
+        return
+      }
+
+      var s = new(IntrospectResponse)
+      err = json.Unmarshal(body, &s)
+      if err != nil {
+        fmt.Printf("%v", err)
+        http.Error(w, "InternalServerError", http.StatusInternalServerError)
+        return
+      }
+
+      if s.Status == false {
+        fmt.Printf("Token %s not active", token)
+        http.Error(w, "Unauthorized", http.StatusUnauthorized)
+        return
+        }
+
+      if s.Status == true {
+        xuid := resp.Header.Get("X-UID")
+
+        // Unlikely to happen, but we check anyway.
+        if xuid == "" {
+          http.Error(w, "InternalServerError", http.StatusInternalServerError)
+          return
+        }
+
+        // TODO:
+        // 
+        // We might need to consider possible collusions if we pick a string
+        // like this.
+        ctx := context.WithValue(r.Context(), "X-UID", xuid)
+        ctx = context.WithValue(ctx, "authToken", token)
+
+        // If token is associated with one or more scopes, put them in 
+        // the context as a slice. 
+        if *s.Scope != "null" {
+          scopes := strings.Split(*s.Scope, " ")
+          ctx = context.WithValue(ctx, "scopes", scopes)
+        }
+
+        // Add ClientID to the context.
+        ctx = context.WithValue(ctx, "ClientID", s.ClientID)
+
+        next.ServeHTTP(w, r.WithContext(ctx))
+        return
+      }
+    })
+}
+
+// TODO Pseudoish. To test.
+func Request(ctx context.Context, r *http.Request) *http.Request {
+  token := BearerToken(ctx)
+  authHeaderVal := "Bearer " + token
+  r.Header.Set("Authorization: ", authHeaderVal)
+  return r
+}
+
+// TODO: To be tested separately.
+func BearerToken(ctx context.Context) string {
+  return Get(ctx, "authToken")
+}
+
+// TODO: To be tested separately.
+func HasScope(ctx context.Context, scope string) bool {
+  scopes, ok := ctx.Value("scopes").([]string)
+
+  if !ok {
+    return false
+  }
+
+  for _, v := range scopes {
+    if v == scope {
+        return true
+    }
+  }
+
+  return false
+}
+
+// TODO: To be tested separately.
+// 
+// Should we propagate an error back to caller? Otherwise this might panic.
+func Get(ctx context.Context, key string) string {
+  return (ctx.Value(key)).(string)
+}
---
+++ oauth2/oauth2_test.go
@@ -0,0 +1,84 @@
+// TODO:
+// 
+// Make sure to use defer close for Closer. 
+// Unify naming structure of context value names.
+package main 
+
+import(
+  "oauth2"
+  "testing"
+  "github.com/gorilla/mux"
+  "net/http"
+  "net/http/httptest"
+)
+
+func dummyHandler(w http.ResponseWriter, r *http.Request) {}
+
+func TestMiddleware(t *testing.T) {
+    // TODO
+    // Run against cp-1-dev or cp-1-prod? 
+    middleware := oauth2.Middleware{
+        Host:         "http://localhost:3000",
+        ClientID:     "13972c02189a6e938a4730bc81c2a20cc4e03ef5406d20d2150110584d6b3e6c",
+        ClientSecret: "7d26f8918a83bd155a936bbe780f32503a88cb8bd3e8acf25248357dff31668e",
+      }
+
+    router := mux.NewRouter()
+    router.Use(middleware.Middleware)
+    router.HandleFunc("/broken", dummyHandler)
+
+    rw := httptest.NewRecorder()
+    req, _ := http.NewRequest("GET", "/broken", nil)
+
+    router.ServeHTTP(rw, req)
+
+    if rw.Body.String() != "Unauthorized\n" {
+        t.Fatalf("Expected `Unauthorized` as response body when *no* token is provided, got %s.", rw.Body)
+    }
+
+    rw = httptest.NewRecorder()
+    req, _ = http.NewRequest("GET", "/broken", nil)
+    req.Header.Set("Authorization Bearer", "sometoken")
+    router.ServeHTTP(rw, req)
+
+    if rw.Body.String() != "Unauthorized\n" {
+        t.Fatalf("Expected `Unauthorized` as response body when *bad* token is provided, got %s.", rw.Body)
+    }
+
+    // Check for data we are interested in inside the context.
+    testMiddlewareHandler := func(w http.ResponseWriter, r *http.Request) {
+        // Check if we have the X-UID.
+        if rw.Result().StatusCode != 200 || r.Context().Value("X-UID") != "3298b629-0467-400e-b430-5259cc3efddc" {
+            t.Fatal("Expected successful request and X-UID stored in request context.")
+        }
+
+        // Check if we have the token.
+        receivedToken := r.Context().Value("authToken").(string)
+        expectedToken := "4905168177b5d81c85092816e1673039176f7a3e1f4d1e8610ec5f5b0ad3f4ef"
+
+        if receivedToken != expectedToken {
+            t.Fatalf("Expected %s, got: %s", expectedToken, receivedToken)
+        }
+
+        // Check if we have the scopes.
+        scopes := r.Context().Value("scopes").([]string)
+        if scopes[0] != "dtc:codes:read" || scopes[1] != "dtc:codes:write" {
+            t.Fatalf("Expected scopes: dtc:codes:read and dtc:codes:write, got: %s", scopes)
+        }
+
+        expectedClientID := "13972c02189a6e938a4730bc81c2a20cc4e03ef5406d20d2150110584d6b3e6c"
+
+        // Check if we have the client ID. 
+        clientID := r.Context().Value("ClientID").(string)
+
+        if clientID != expectedClientID {
+            t.Fatalf("Expected ClientID 6, got: %s", clientID)
+        }
+    }
+
+    rw = httptest.NewRecorder()
+    req, _ = http.NewRequest("GET", "/working", nil)
+    req.Header.Set("Authorization", "Bearer 4905168177b5d81c85092816e1673039176f7a3e1f4d1e8610ec5f5b0ad3f4ef")
+    router.HandleFunc("/working", testMiddlewareHandler)
+    router.ServeHTTP(rw, req)
+}
