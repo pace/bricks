0a4f711aa5567c297c50922d7ffaec0569e5d13d
Vincent Landgraf <vincent@pace.car>
2018-09-05 10:31:59 +0200

    adds regular go collector, process collector as well as a Prometheus handler
    #6

---
+++ maintenance/metrics/handler_test.go
@@ -0,0 +1,22 @@
+// Copyright © 2018 by PACE Telematics GmbH. All rights reserved.
+// Created at 2018/09/05 by Vincent Landgraf
+
+package metrics
+
+import (
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	rec := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/metrics", nil)
+
+	Handler().ServeHTTP(rec, req)
+
+	resp := rec.Result()
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Failed to respond with prometheus metrics: %v", resp.StatusCode)
+	}
+}
