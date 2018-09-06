3f5b969a0b39eec14720ea6749395c5aea5274cc
Vincent Landgraf <vincent@pace.car>
2018-09-06 11:26:35 +0200

    fixes typo
    #13

--- http/doc.go
+++ http/doc.go
@@ -1,6 +1,6 @@
 // Copyright © 2018 by PACE Telematics GmbH. All rights reserved.
 // Created at 2018/09/06 by Vincent Landgraf
 
-// Package http implement the basic pace mircoservice handlers
+// Package http implements the basic pace microservice handlers
 // and a standard microservice server configured via environment variables.
 package http
