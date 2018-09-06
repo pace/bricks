bd7d9e967cad1a484782fd725197c7dfbad52fc8
Vincent Landgraf <vincent@pace.car>
2018-09-06 15:29:52 +0200

    use iso8601 and UTC
    #3

--- maintenance/log/log.go
+++ maintenance/log/log.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/rs/zerolog/hlog"
 
@@ -17,12 +18,16 @@ import (
 )
 
 func init() {
+	// use ico8601 (and UTC for json) as defined in https://lab.jamit.de/pace/web/meta/issues/11
+	zerolog.TimeFieldFormat = "2006-01-02 15:04:05"
+
 	// if it is a terminal use the console writer
 	if isatty.IsTerminal(os.Stdout.Fd()) {
 		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout})
 		return
 	}
 	log.Logger = log.Logger.Output(os.Stdout)
+	zerolog.TimestampFunc = func() time.Time { return time.Now().UTC() }
 }
 
 // Req returns the logger for the passed request
