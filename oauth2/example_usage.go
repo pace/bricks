ddd2e34e4f9543f0131b28b60cdfb24862292aee
Mohamed Wael Khobalatte <wael@pacelink.de>
2018-09-09 19:06:44 +0200

    Run gofmt on oauth2 package

--- oauth2/example_usage.go
+++ oauth2/example_usage.go
@@ -1,38 +1,38 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "github.com/gorilla/mux"
-  "oauth2"
-  "net/http"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"oauth2"
 )
 
 func main() {
-  r := mux.NewRouter()
-  middleware := oauth2.Middleware{
-    Host:         "http://localhost:3000",
-    ClientID:     "13972c02189a6e938a4730bc81c2a20cc4e03ef5406d20d2150110584d6b3e6c",
-    ClientSecret: "7d26f8918a83bd155a936bbe780f32503a88cb8bd3e8acf25248357dff31668e",
-  }
+	r := mux.NewRouter()
+	middleware := oauth2.Middleware{
+		Host:         "http://localhost:3000",
+		ClientID:     "13972c02189a6e938a4730bc81c2a20cc4e03ef5406d20d2150110584d6b3e6c",
+		ClientSecret: "7d26f8918a83bd155a936bbe780f32503a88cb8bd3e8acf25248357dff31668e",
+	}
 
-  r.Use(middleware.Middleware)
-  
-  r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    log.Printf("AUDIT: User %s does something", oauth2.Get(r.Context(), "X-UID"))
+	r.Use(middleware.Middleware)
 
-    if oauth2.HasScope(r.Context(), "dtc:codes:write") {
-      fmt.Fprintf(w, "User has scope.")
-      return
-    }
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("AUDIT: User %s does something", oauth2.Get(r.Context(), "X-UID"))
 
-    fmt.Fprintf(w, "Your client may not have the right scopes to see the secret code")
-  })
+		if oauth2.HasScope(r.Context(), "dtc:codes:write") {
+			fmt.Fprintf(w, "User has scope.")
+			return
+		}
 
-  srv := &http.Server{
-    Handler: r,
-    Addr:    "127.0.0.1:8000",
-  }
+		fmt.Fprintf(w, "Your client may not have the right scopes to see the secret code")
+	})
 
-  log.Fatal(srv.ListenAndServe())
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "127.0.0.1:8000",
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }
