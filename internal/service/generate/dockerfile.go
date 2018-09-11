90d0ffb9214df5f09676591709bdd8144e19b1ad
Florian Hübsch <flo@pace.car>
2018-09-11 13:44:25 +0200

    Generate Dockerfile with jaeger service name
    Closes #5

--- internal/service/generate/dockerfile.go
+++ internal/service/generate/dockerfile.go
@@ -54,5 +54,6 @@ COPY --from=builder /go/bin/{{ .Commands.ControlName }} /usr/local/bin/
 
 EXPOSE 3000
 ENV PORT 3000
+ENV JAEGER_SERVICE_NAME {{ .Name }}
 CMD /usr/local/bin/{{ .Commands.DaemonName }}
 `))
