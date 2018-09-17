d26c4f60a1357e629b7ba538de46f3a85e6709b5
Mohamed Wael Khobalatte <wael@pacelink.de>
2018-09-17 12:00:00 -0400

    Test accessors fully

--- oauth2/oauth2_test.go
+++ oauth2/oauth2_test.go
@@ -72,3 +72,76 @@ func TestRequestWithNoToken(t *testing.T) {
 		t.Fatalf("Expected request to have no authorization header, got: %v", header)
 	}
 }
+
+func TestSuccessfulAccessors(t *testing.T) {
+	expectedBearerToken := "somevalue"
+	expectedUserID := "someuserid"
+	expectedClientID := "someclientid"
+	expectedScopes := []string{"scope1", "scope2"}
+
+	var to = token{
+		value:    expectedBearerToken,
+		userID:   expectedUserID,
+		clientID: expectedClientID,
+		scopes:   expectedScopes,
+	}
+
+	ctx := context.WithValue(context.TODO(), tokenKey, &to)
+
+	uid, _ := UserID(ctx)
+	cid, _ := ClientID(ctx)
+	bearerToken, _ := BearerToken(ctx)
+	scopes := Scopes(ctx)
+	hasScope := HasScope(ctx, "scope2")
+
+	if uid != expectedUserID {
+		t.Fatalf("Expected %v, got: %v", expectedUserID, uid)
+	}
+
+	if cid != expectedClientID {
+		t.Fatalf("Expected %v, got: %v", expectedClientID, cid)
+	}
+
+	if bearerToken != expectedBearerToken {
+		t.Fatalf("Expected %v, got: %v", expectedBearerToken, bearerToken)
+	}
+
+	if scopes[0] != "scope1" || scopes[1] != "scope2" {
+		t.Fatalf("Expected %v, got: %v", expectedScopes, scopes)
+	}
+
+	if hasScope != true {
+		t.Fatalf("Expected true, got: %v", hasScope)
+	}
+}
+
+// Ensure we return sensible results when no data is present, and not panic.
+func TestUnsucessfulAccessors(t *testing.T) {
+	ctx := context.TODO()
+
+	uid, uidOK := UserID(ctx)
+	cid, cidOK := ClientID(ctx)
+	bt, btOK := BearerToken(ctx)
+	scopes := Scopes(ctx)
+	hasScope := HasScope(ctx, "scope2")
+
+	if uid != "" || uidOK != false {
+		t.Fatalf("Expected no %v, got: %v", "UserID", uid)
+	}
+
+	if cid != "" || cidOK != false {
+		t.Fatalf("Expected no %v, got: %v", "ClientID", cid)
+	}
+
+	if bt != "" || btOK != false {
+		t.Fatalf("Expected no %v, got: %v", "BearerToken", bt)
+	}
+
+	if len(scopes) > 0 {
+		t.Fatalf("Expected no scopes, got: %v", scopes)
+	}
+
+	if hasScope != false {
+		t.Fatalf("Expected hasScope to return false, got: %v", hasScope)
+	}
+}
