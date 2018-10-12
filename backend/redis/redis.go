e182682bf29e95f0b203db77993f29c516eaadbb
Vincent Landgraf <vincent@pace.car>
2018-10-12 16:53:15 +0200

    fixes redis err reporting #29

--- backend/redis/redis.go
+++ backend/redis/redis.go
@@ -141,7 +141,7 @@ func (lt *logtracer) handle(realProcess func(redis.Cmder) error) func(redis.Cmde
 
 		// add error
 		if err != nil {
-			span.LogFields(olog.Err(err))
+			span.LogFields(olog.Error(err))
 			le = le.Err(err)
 		}
 
