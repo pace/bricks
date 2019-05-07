2c4500891a1e7f033d9dba100d64b5f0857839ff
Marius Neugebauer <marius.neugebauer@pacetelematics.com>
2019-05-07 12:30:26 +0200

    jsonapi: fix test #39

--- maintenance/metric/jsonapi/jsonapi_test.go
+++ maintenance/metric/jsonapi/jsonapi_test.go
@@ -12,36 +12,38 @@ import (
 	"github.com/pace/bricks/maintenance/metric"
 )
 
-func TestCaptureStatus(t *testing.T) {
-	rec := httptest.NewRecorder()
-	req := httptest.NewRequest("GET", "/test/1234567", nil)
+func TestMetric(t *testing.T) {
+	t.Run("capture metrics", func(t *testing.T) {
+		rec := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", "/test/1234567", nil)
 
-	handler := func(w http.ResponseWriter, r *http.Request) {
-		w = NewMetric("simple", "/test/{id}", w, r)
-		w.WriteHeader(204)
-	}
-
-	handler(rec, req)
-
-	resp := rec.Result()
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 204 {
-		t.Errorf("Failed to return correct 204 response status, got: %v", resp.StatusCode)
-	}
-
-	rec = httptest.NewRecorder()
-	req = httptest.NewRequest("GET", "/metrics", nil)
-	metric.Handler().ServeHTTP(rec, req)
-
-	body := rec.Body.String()
-	for _, metric := range []string{
-		"pace_api_http_request_duration",
-		"pace_api_http_request_duration_seconds",
-		"pace_api_http_size_bytes",
-	} {
-		if !strings.Contains(body, metric) {
-			t.Errorf("Expected pace api metrics got: %v", body)
+		handler := func(w http.ResponseWriter, r *http.Request) {
+			w = NewMetric("simple", "/test/{id}", w, r)
+			w.WriteHeader(204)
 		}
-	}
+
+		handler(rec, req)
+
+		resp := rec.Result()
+		defer resp.Body.Close()
+
+		if resp.StatusCode != 204 {
+			t.Errorf("Failed to return correct 204 response status, got: %v", resp.StatusCode)
+		}
+
+		rec = httptest.NewRecorder()
+		req = httptest.NewRequest("GET", "/metrics", nil)
+		metric.Handler().ServeHTTP(rec, req)
+
+		body := rec.Body.String()
+		for _, metric := range []string{
+			"pace_api_http_request_total",
+			"pace_api_http_request_duration_seconds",
+			"pace_api_http_size_bytes",
+		} {
+			if !strings.Contains(body, metric) {
+				t.Errorf("Expected pace api metrics got: %v", body)
+			}
+		}
+	})
 }
