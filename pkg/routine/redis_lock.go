bbffd4018fa672bb1c78fa16a158bbc8639d4de0
Marius Neugebauer <marius.neugebauer@pacetelematics.com>
2020-03-09 12:13:09 +0100

    routine: handle lock errors gracefully

--- pkg/routine/redis_lock.go
+++ pkg/routine/redis_lock.go
@@ -5,12 +5,15 @@ package routine
 
 import (
 	"context"
+	"fmt"
 	"sync"
 	"time"
 
 	"github.com/bsm/redislock"
 	"github.com/go-redis/redis"
 	redisbackend "github.com/pace/bricks/backend/redis"
+	"github.com/pace/bricks/maintenance/errors"
+	"github.com/pace/bricks/maintenance/log"
 )
 
 func routineThatKeepsRunningOneInstance(name string, routine func(context.Context)) func(context.Context) {
@@ -23,8 +26,10 @@ func routineThatKeepsRunningOneInstance(name string, routine func(context.Contex
 				return
 			case <-time.After(tryAgainIn):
 			}
-			lockCtx := obtainLock(ctx, locker, "routine:lock:"+name, cfg.RedisLockTTL)
-			if lockCtx != nil {
+			lockCtx, err := obtainLock(ctx, locker, "routine:lock:"+name, cfg.RedisLockTTL)
+			if err != nil {
+				go errors.Handle(ctx, err) // report error to Sentry, non-blocking
+			} else if lockCtx != nil {
 				routine(lockCtx)
 			}
 			tryAgainIn = cfg.RedisLockTTL / 5
@@ -44,34 +49,38 @@ func getDefaultRedisClient() *redis.Client {
 
 // Try to obtain a lock. Return a sub-context of ctx that is canceled once the
 // lock is lost or ctx is done.
-func obtainLock(ctx context.Context, locker *redislock.Client, key string, ttl time.Duration) context.Context {
+func obtainLock(ctx context.Context, locker *redislock.Client, key string, ttl time.Duration) (context.Context, error) {
+	num := ctx.Value(ctxNumKey{}).(int64)
+
 	// obtain lock
 	lock, err := locker.Obtain(key, ttl, nil)
 	if err == redislock.ErrNotObtained {
-		return nil
+		return nil, nil
 	} else if err != nil {
-		panic(err)
+		log.Ctx(ctx).Debug().Err(err).Msg("could not obtain lock")
+		return nil, err
 	}
 
 	// keep up lock, cancel lockCtx otherwise
 	lockCtx, cancel := context.WithCancel(ctx)
 	go func() {
+		defer errors.HandleWithCtx(ctx, fmt.Sprintf("routine %d: keep up lock", num)) // handle panics
 		keepUpLock(ctx, lock, ttl)
 		cancel()
 		err := lock.Release()
 		if err != nil && err != redislock.ErrLockNotHeld {
-			panic(err)
+			log.Ctx(ctx).Debug().Err(err).Msg("could not release lock")
 		}
 	}()
 
-	return lockCtx
+	return lockCtx, nil
 }
 
 // Try to keep up a lock for as long as the context is valid. Return once the
 // lock is lost or the context is done.
 func keepUpLock(ctx context.Context, lock *redislock.Lock, refreshTTL time.Duration) {
 	refreshInterval := refreshTTL / 5
-	lockRunsOutIn := refreshTTL // initial value must be > refresh interval
+	lockRunsOutIn := refreshTTL // initial value after obtaining the lock
 	for {
 		select {
 		case <-ctx.Done():
@@ -89,11 +98,13 @@ func keepUpLock(ctx context.Context, lock *redislock.Lock, refreshTTL time.Durat
 			// Don't return just yet. Get the TTL of the lock and try to
 			// refresh for as long as the TTL is not over.
 			if lockRunsOutIn, err = lock.TTL(); err != nil {
-				panic(err)
+				log.Ctx(ctx).Debug().Err(err).Msg("could not get ttl of lock")
+				return // assuming we lost the lock
 			}
 			continue
 		} else if err != nil {
-			panic(err)
+			log.Ctx(ctx).Debug().Err(err).Msg("could not refresh lock")
+			return // assuming we lost the lock
 		}
 		// reset, because the lock was refreshed
 		lockRunsOutIn = refreshTTL
