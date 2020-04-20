9029cd15e1ce60bded39e6880ad487c3576671cc
Nico Muerdter <nicomuerdter@gmail.com>
2020-04-20 15:04:38 +0200

    Silence /metrics logs

--- http/router.go
+++ http/router.go
@@ -24,10 +24,11 @@ func Router() *mux.Router {
 
 	r.Use(metricsMiddleware)
 
-	// the logging middleware needs to be registered before the error
-	// middleware to make it possible to send panics to sentry.
-	// "/health" is only logged to the log.Sink but not to log.output (silent)
-	r.Use(log.Handler("/health"))
+	// the logging middleware needs to be registered before the
+	// error middleware to make it possible to send panics to
+	// sentry. "/health" and "/metrics" are only logged to the
+	// log.Sink but not to log.output (silent)
+	r.Use(log.Handler("/health", "/metrics"))
 
 	// last resort error handler
 	r.Use(errors.Handler())
