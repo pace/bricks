f9b3396eeb74f8703a29014f06568d28d07929ee
Julius Foitzik <info@accountr.eu>
2020-10-21 16:23:59 +0200

    Objstore: Remove healthchecks from init() and wrap in sync.Once #235

--- backend/objstore/objstore.go
+++ backend/objstore/objstore.go
@@ -2,11 +2,13 @@ package objstore
 
 import (
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/caarlos0/env"
 	"github.com/minio/minio-go/v6"
 	"github.com/minio/minio-go/v6/pkg/credentials"
+
 	"github.com/pace/bricks/http/transport"
 	"github.com/pace/bricks/maintenance/health/servicehealthcheck"
 	"github.com/pace/bricks/maintenance/log"
@@ -26,35 +28,19 @@ type config struct {
 
 var cfg config
 
-func init() {
-	// parse log config
-	err := env.Parse(&cfg)
-	if err != nil {
-		log.Fatalf("Failed to parse object storage environment: %v", err)
-	}
-
-	client, err := Client()
-	if err != nil {
-		log.Fatalf("Failed to create object storage client: %v", err)
-	}
-	servicehealthcheck.RegisterHealthCheck("objstore", &HealthCheck{
-		Client: client,
-	})
-
-	ok, err := client.BucketExists(cfg.HealthCheckBucketName)
-	if err != nil {
-		log.Warnf("Failed to create check for bucket: %v", err)
-	}
-	if !ok {
-		err := client.MakeBucket(cfg.HealthCheckBucketName, cfg.Region)
-		if err != nil {
-			log.Warnf("Failed to create bucket: %v", err)
-		}
-	}
+func RegisterHealthchecks() {
+	registerHealthchecks()
 }
 
-// Client with environment based configuration
+// deprecated consider using DefaultClientFromEnv
 func Client() (*minio.Client, error) {
+	return DefaultClientFromEnv()
+}
+
+// Client with environment based configuration. Registers healthchecks automatically. If yo do not want to use healthchecks
+// consider calling CustomClient.
+func DefaultClientFromEnv() (*minio.Client, error) {
+	registerHealthchecks()
 	return CustomClient(cfg.Endpoint, &minio.Options{
 		Secure:       cfg.UseSSL,
 		Region:       cfg.Region,
@@ -75,6 +61,46 @@ func CustomClient(endpoint string, opts *minio.Options) (*minio.Client, error) {
 		Msg("S3 connection created")
 	client.SetCustomTransport(newCustomTransport(endpoint))
 	return client, nil
+}
+
+func client() (*minio.Client, error) {
+	return CustomClient(cfg.Endpoint, &minio.Options{
+		Secure:       cfg.UseSSL,
+		Region:       cfg.Region,
+		BucketLookup: minio.BucketLookupAuto,
+		Creds:        credentials.NewStaticV4(cfg.AccessKeyID, cfg.SecretAccessKey, ""),
+	})
+}
+
+var register = &sync.Once{}
+
+func registerHealthchecks() {
+	register.Do(func() {
+		// parse log config
+		err := env.Parse(&cfg)
+		if err != nil {
+			log.Fatalf("Failed to parse object storage environment: %v", err)
+		}
+
+		client, err := client()
+		if err != nil {
+			log.Fatalf("Failed to create object storage client: %v", err)
+		}
+		servicehealthcheck.RegisterHealthCheck("objstore", &HealthCheck{
+			Client: client,
+		})
+
+		ok, err := client.BucketExists(cfg.HealthCheckBucketName)
+		if err != nil {
+			log.Warnf("Failed to create check for bucket: %v", err)
+		}
+		if !ok {
+			err := client.MakeBucket(cfg.HealthCheckBucketName, cfg.Region)
+			if err != nil {
+				log.Warnf("Failed to create bucket: %v", err)
+			}
+		}
+	})
 }
 
 func newCustomTransport(endpoint string) http.RoundTripper {
