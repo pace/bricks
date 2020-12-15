68414d9c2e63b0f39db8989263f056b7c3a9493c
Vincent Landgraf <vincent@pacetelematics.com>
2020-12-15 11:22:25 +0100

    measure and pickup external http dependencies

--- http/transport/default_transport.go
+++ http/transport/default_transport.go
@@ -7,6 +7,22 @@ package transport
 // If not explicitly finalized via `Final` it uses `http.DefaultTransport` as finalizer.
 func NewDefaultTransportChain() *RoundTripperChain {
 	return Chain(
+		&ExternalDependencyRoundTripper{},
+		NewDefaultRetryRoundTripper(),
+		&JaegerRoundTripper{},
+		NewDumpRoundTripperEnv(),
+		&LoggingRoundTripper{},
+		&LocaleRoundTripper{},
+		&RequestIDRoundTripper{},
+	)
+}
+
+// NewDefaultTransportChain returns a transport chain with retry, jaeger and logging support.
+// If not explicitly finalized via `Final` it uses `http.DefaultTransport` as finalizer.
+// The passed name is recorded as external dependency
+func NewDefaultTransportChainWithExternalName(name string) *RoundTripperChain {
+	return Chain(
+		&ExternalDependencyRoundTripper{name: name},
 		NewDefaultRetryRoundTripper(),
 		&JaegerRoundTripper{},
 		NewDumpRoundTripperEnv(),
