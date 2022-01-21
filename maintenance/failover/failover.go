6ec88f4a59ff466ad8c4d4de994ec1ce9c799097
Vincent Landgraf <vincent@pacetelematics.com>
2022-01-21 11:14:49 +0100

    manually read files

--- maintenance/failover/failover.go
+++ maintenance/failover/failover.go
@@ -41,11 +41,11 @@ const Label = "github.com.pace.bricks.activepassive"
 // Config gathers the required kubernetes system configuration to use the
 // kubernetes API
 type Config struct {
-	Host      string `env:"KUBERNETES_SERVICE_HOST" envDefault:"localhost"`
-	Port      int    `env:"KUBERNETES_PORT_443_TCP_PORT" envDefault:"433"`
-	Namespace string `env:"KUBERNETES_NAMESPACE,file" envDefault:"/run/secrets/kubernetes.io/serviceaccount/namespace"`
-	CACert    []byte `env:"KUBERNETES_API_CA,file" envDefault:"/run/secrets/kubernetes.io/serviceaccount/ca.crt"`
-	Token     string `env:"KUBERNETES_API_TOKEN,file" envDefault:"/run/secrets/kubernetes.io/serviceaccount/token"`
+	Host          string `env:"KUBERNETES_SERVICE_HOST" envDefault:"localhost"`
+	Port          int    `env:"KUBERNETES_PORT_443_TCP_PORT" envDefault:"433"`
+	NamespaceFile string `env:"KUBERNETES_NAMESPACE_FILE" envDefault:"/run/secrets/kubernetes.io/serviceaccount/namespace"`
+	CACertFile    string `env:"KUBERNETES_API_CA_FILE" envDefault:"/run/secrets/kubernetes.io/serviceaccount/ca.crt"`
+	TokenFile     string `env:"KUBERNETES_API_TOKEN_FILE" envDefault:"/run/secrets/kubernetes.io/serviceaccount/token"`
 }
 
 // ActivePassive implements a failover mechanism that allows
@@ -76,6 +76,9 @@ type ActivePassive struct {
 	locker         *redislock.Client
 	cfg            Config
 	podname        string
+	Namespace      string
+	CACert         []byte
+	Token          string
 
 	state   status
 	stateMu sync.RWMutex
@@ -110,10 +113,28 @@ func NewActivePassive(clusterName string, timeToFailover time.Duration, client *
 		return nil, err
 	}
 
+	caData, err := os.ReadFile(ap.cfg.CACertFile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read %q: %v", ap.cfg.CACertFile, err)
+	}
+	ap.CACert = []byte(strings.TrimSpace(string(caData)))
+
+	namespaceData, err := os.ReadFile(ap.cfg.NamespaceFile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read %q: %v", ap.cfg.NamespaceFile, err)
+	}
+	ap.Namespace = strings.TrimSpace(string(namespaceData))
+
+	tokenData, err := os.ReadFile(ap.cfg.TokenFile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read %q: %v", ap.cfg.CACertFile, err)
+	}
+	ap.Token = strings.TrimSpace(string(tokenData))
+
 	// add kubernetes api server cert
 	chain := transport.NewDefaultTransportChain()
 	pool := x509.NewCertPool()
-	ok := pool.AppendCertsFromPEM(ap.cfg.CACert)
+	ok := pool.AppendCertsFromPEM(ap.CACert)
 	if !ok {
 		return nil, fmt.Errorf("failed to load kubernetes ca cert")
 	}
@@ -289,7 +310,7 @@ start:
 	}
 
 	url := fmt.Sprintf("https://%s:%d/api/v1/namespaces/%s/pods/%s",
-		a.cfg.Host, a.cfg.Port, a.cfg.Namespace, a.podname)
+		a.cfg.Host, a.cfg.Port, a.Namespace, a.podname)
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodPatch, url, bytes.NewReader(data))
 	if err != nil {
@@ -297,7 +318,7 @@ start:
 	}
 
 	req.Header.Set("Content-Type", "application/json-patch+json")
-	req.Header.Set("Authorization", "Bearer "+a.cfg.Token)
+	req.Header.Set("Authorization", "Bearer "+a.Token)
 
 	resp, err := a.Client.Do(req)
 	if err != nil {
