9d4a15ddf31286707f71bc8f82f413c21e24c743
Julius Foitzik <info@accountr.eu>
2022-04-07 12:48:30 +0200

    couchdb,http: Allow to disable logging transport in CouchDB client

--- backend/couchdb/couchdb.go
+++ backend/couchdb/couchdb.go
@@ -6,12 +6,13 @@ package couchdb
 import "time"
 
 type Config struct {
-	URL                  string        `env:"COUCHDB_URL" envDefault:"http://couchdb:5984/"`
-	User                 string        `env:"COUCHDB_USER" envDefault:"admin"`
-	Password             string        `env:"COUCHDB_PASSWORD" envDefault:"secret"`
-	Database             string        `env:"COUCHDB_DB" envDefault:"test"`
-	DatabaseAutoCreate   bool          `env:"COUCHDB_DB_AUTO_CREATE" envDefault:"true"`
-	HealthCheckKey       string        `env:"COUCHDB_HEALTH_CHECK_KEY" envDefault:"$health_check"`
-	HealthCheckResultTTL time.Duration `env:"COUCHDB_HEALTH_CHECK_RESULT_TTL" envDefault:"10s"`
-	DisableHealthCheck   bool          `env:"COUCHDB_DISABLE_HEALTH_CHECK" envDefault:"false"`
+	URL                   string        `env:"COUCHDB_URL" envDefault:"http://couchdb:5984/"`
+	User                  string        `env:"COUCHDB_USER" envDefault:"admin"`
+	Password              string        `env:"COUCHDB_PASSWORD" envDefault:"secret"`
+	Database              string        `env:"COUCHDB_DB" envDefault:"test"`
+	DatabaseAutoCreate    bool          `env:"COUCHDB_DB_AUTO_CREATE" envDefault:"true"`
+	HealthCheckKey        string        `env:"COUCHDB_HEALTH_CHECK_KEY" envDefault:"$health_check"`
+	HealthCheckResultTTL  time.Duration `env:"COUCHDB_HEALTH_CHECK_RESULT_TTL" envDefault:"10s"`
+	DisableHealthCheck    bool          `env:"COUCHDB_DISABLE_HEALTH_CHECK" envDefault:"false"`
+	DisableRequestLogging bool          `env:"COUCHDB_DB_DISABLE_REQUEST_LOGGING" envDefault:"false"`
 }
