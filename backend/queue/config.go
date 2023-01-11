27681d0bade1a2a15de4e1a318422623878d836b
Arnold Iakab <arnold.iakab@tremend.com>
2023-01-11 19:31:27 +0200

    Implement pending state for queue healthcheck

--- backend/queue/config.go
+++ backend/queue/config.go
@@ -8,8 +8,13 @@ import (
 )
 
 type config struct {
-	HealthCheckResultTTL   time.Duration `env:"RMQ_HEALTH_CHECK_RESULT_TTL" envDefault:"10s"`
-	MetricsRefreshInterval time.Duration `env:"RMQ_METRICS_REFRESH_INTERVAL" envDefault:"10s"`
+	HealthCheckResultTTL time.Duration `env:"RMQ_HEALTH_CHECK_RESULT_TTL" envDefault:"10s"`
+	// HealthCheckPendingStateInterval represents the time between a queue becoming unhealthy and marking it as unhealthy.
+	// Used to prevent a queue from becoming immediately unhealthy when a surge of deliveries occurs,
+	// providing time to the service that uses this implementation to deal with the sudden increase in work without being
+	// signaled as unhealthy.
+	HealthCheckPendingStateInterval time.Duration `env:"RMQ_HEALTH_CHECK_PENDING_STATE_INTERVAL" envDefault:"1m"`
+	MetricsRefreshInterval          time.Duration `env:"RMQ_METRICS_REFRESH_INTERVAL" envDefault:"10s"`
 }
 
 var cfg config
