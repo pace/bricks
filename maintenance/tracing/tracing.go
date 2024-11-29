073cc28c952fa5902a49916cea1432caa35eafc7
Thomas Hipp <thomas.hipp@pace.car>
2024-11-29 10:37:33 +0100

    tracing: Update options

--- maintenance/tracing/tracing.go
+++ maintenance/tracing/tracing.go
@@ -3,9 +3,9 @@
 package tracing
 
 import (
-	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/getsentry/sentry-go"
 	"github.com/pace/bricks/maintenance/log"
@@ -32,32 +32,39 @@ type traceHandler struct {
 // Trace the service function handler execution
 func (h *traceHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-
-	hub := sentry.GetHubFromContext(ctx)
-	if hub == nil {
-		// Check the concurrency guide for more details: https://docs.sentry.io/platforms/go/concurrency/
-		hub = sentry.CurrentHub().Clone()
-		ctx = sentry.SetHubOnContext(ctx, hub)
-	}
+	hub := sentry.CurrentHub()
 
 	options := []sentry.SpanOption{
-		// Set the OP based on values from https://develop.sentry.dev/sdk/performance/span-operations/
+		sentry.ContinueTrace(hub, r.Header.Get(sentry.SentryTraceHeader), r.Header.Get(sentry.SentryBaggageHeader)),
 		sentry.WithOpName("http.server"),
-		sentry.ContinueFromRequest(r),
 		sentry.WithTransactionSource(sentry.SourceURL),
+		sentry.WithSpanOrigin(sentry.SpanOriginStdLib),
 	}
 
-	span := sentry.StartTransaction(ctx,
-		fmt.Sprintf("%s %s", r.Method, r.URL.Path),
+	transaction := sentry.StartTransaction(ctx,
+		getHTTPSpanName(r),
 		options...,
 	)
+	transaction.SetData("http.request.method", r.Method)
 
-	defer span.Finish()
-
-	ctx = span.Context()
 	ww := mutil.WrapWriter(w)
 
-	h.next.ServeHTTP(ww, r.WithContext(ctx))
+	defer func() {
+		status := ww.Status()
+		bytesWritten := ww.BytesWritten()
+		transaction.Status = sentry.HTTPtoSpanStatus(status)
+
+		transaction.SetData("http.response.status_code", status)
+		transaction.SetData("http.response.content_length", bytesWritten)
+		transaction.SetData("http.request.url", r.URL.String())
+
+		transaction.Finish()
+	}()
+
+	hub.Scope().SetRequest(r)
+	r = r.WithContext(transaction.Context())
+
+	h.next.ServeHTTP(ww, r)
 }
 
 // Handler generates a tracing handler that decodes the current trace from the wire.
@@ -102,4 +109,19 @@ func TraceLogHandler(ignoredPrefixes ...string) func(http.Handler) http.Handler 
 			next: next,
 		}
 	}, ignoredPrefixes...)
+}
+
+// getHTTPSpanName grab needed fields from *http.Request to generate a span name for `http.server` span op.
+func getHTTPSpanName(r *http.Request) string {
+	if r.Pattern != "" {
+		// If value does not start with HTTP methods, add them.
+		// The method and the path should be separated by a space.
+		if parts := strings.SplitN(r.Pattern, " ", 2); len(parts) == 1 {
+			return r.Method + " " + r.Pattern
+		}
+
+		return r.Pattern
+	}
+
+	return r.Method + " " + r.URL.Path
 }
