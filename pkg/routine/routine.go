86ea61463b40887615627d8b9c3b7229612f5191
Thomas Hipp <thomas.hipp@pace.car>
2024-12-04 12:48:25 +0100

    pkg/routine: Use WithoutCancel instead of Transfer

--- pkg/routine/routine.go
+++ pkg/routine/routine.go
@@ -15,7 +15,6 @@ import (
 	"github.com/getsentry/sentry-go"
 	"github.com/pace/bricks/maintenance/errors"
 	"github.com/pace/bricks/maintenance/log"
-	pkgcontext "github.com/pace/bricks/pkg/context"
 )
 
 type options struct {
@@ -87,8 +86,8 @@ func RunNamed(parentCtx context.Context, name string, routine func(context.Conte
 // thrown in the function are logged and sent to sentry. The routines context is
 // canceled if the program receives a shutdown signal (SIGINT, SIGTERM), if the
 // returned CancelFunc is called, or if the routine returned.
-func Run(parentCtx context.Context, routine func(context.Context)) (cancel context.CancelFunc) {
-	ctx := pkgcontext.Transfer(parentCtx)
+func Run(ctx context.Context, routine func(context.Context)) (cancel context.CancelFunc) {
+	ctx = context.WithoutCancel(ctx)
 
 	// add routine number to context and logger
 	num := atomic.AddInt64(&ctr, 1)
